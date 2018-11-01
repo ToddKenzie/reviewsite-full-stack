b27534bc20e9804329431858644484d9340b4a28
Todd <mopeybear@gmail.com>
2018-11-01 11:11:15 -0400

    Review Class framework

---
+++ src/main/java/module7/reviewsitefullstack/ReviewRepository.java
@@ -0,0 +1,7 @@
+package module7.reviewsitefullstack;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository extends CrudRepository<Review, Long>{
+
+}
---
+++ src/test/java/module7/reviewsitefullstack/ReviewRepositoryTest.java
@@ -0,0 +1,63 @@
+package module7.reviewsitefullstack;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class ReviewRepositoryTest {
+	
+	@Resource
+	private TestEntityManager entity;
+	
+	@Resource
+	private ReviewRepository reviewRepo;
+	
+	Long reviewId;
+	Review review1;
+	String reviewText;
+	
+	@Before
+	public void setUp() {
+		reviewText = "This is test text";
+		review1 = reviewRepo.save(new Review(reviewText));
+		reviewId = review1.getId();
+		
+		entity.flush();
+		entity.clear();
+	}
+	
+	
+	@Test
+	public void verifyReviewHasID() {
+		assertThat(reviewId, greaterThan(0L));
+	}
+	
+	@Test
+	public void shouldSaveAndLoadTag() {	
+		Optional<Review> underTest = reviewRepo.findById(reviewId);
+		Review testReview = underTest.get();
+		assertThat(testReview, is(review1));
+	}
+	
+	@Test
+	public void reviewShouldContainText() {
+		Optional<Review> underTest = reviewRepo.findById(reviewId);
+		String testReviewText = underTest.get().getText();
+		assertThat(testReviewText, is(reviewText));
+		
+	}
+
+}
